f9812f39404e7d5b61a4d478cfa88ac8c78e594d
vcampos2017 <vincentpcampos@gmail.com>
2017-06-04 08:42:36 -0500

    Create MiniWeather2.py
    This is a revision to version one with barometric sensor and sending data to Ubidots

---
+++ MiniWeather2.py
@@ -0,0 +1,59 @@
+from OmegaExpansion import oledExp
+#https://www.controleverything.com/content/Humidity
+from OmegaExpansion import onionI2C
+# MiniWeather tests running two ControlEverything Sensors from One Board
+# Results nare printed to the screen
+import os
+import time
+import datetime
+import sys
+import requests
+
+# This sets the tokens for sending data to Ubidots
+DEVICE="My-Devince-Name"
+TOKEN="My=Special-Token"
+
+#Start OLED
+oledExp.driverInit()
+
+while True:
+        # Get I2C bus
+        i2c 	= onionI2C.OnionI2C(0)
+        # set the verbosity
+        i2c.setVerbosity(1)
+        # HIH7130 address, 0x27(39)
+        # Read data back from 0x00(00), 4 bytes
+        # humidity MSB, humidity LSB, temp MSB, temp LSB
+        data = i2c.readBytes(0x27, 0x00, 4)
+        # Convert the data to 14-bits
+        humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 100.0) / 16383.0
+        temp = (((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4
+        cTemp = (temp / 16384.0) * 165.0 - 40.
+        fTemp = cTemp * 1.8 + 32
+        # LPS25HB address, 0x5C(92)
+        # Select Control register, 0x20(32)
+        # 0x90(144)	Active mode, Continous update
+        i2c.writeByte(0x5C, 0x20, 0x90)
+        time.sleep(0.1)
+        # LPS25HB address, 0x5C(92)
+        # Read data back from 0x28(40), with Command register, 0x80(128)
+        # 3 bytes, Pressure LSB first
+        data =i2c.readBytes(0x5C, 0x28 | 0x80, 3)
+        # Convert the data to hPa
+        pressure = (data[2] * 65536 + data[1] * 256 + data[0]) / 4096.0
+        # Output data to screen with pause between in print/write
+        print time.strftime("%H:%M:%S")
+        time.sleep(3)
+        print "Relative Humidity : %.2f %%" %humidity
+        time.sleep(3)
+        print "Temperature in Celsius : %.2f C" %cTemp
+        time.sleep(3)
+        print "Barometric Pressure is : %.2f hPa" %pressure
+        print "Temperature in Fahrenheit : %.2f F" %fTemp
+        #Send Data to Ubidots
+        payload = {'temperature': fTemp, 'humidity': humidity, 'barometricpressure':pressure }
+        r=requests.post('http://things.ubidots.com/api/v1.6/devices/DEVICE/?token=TOKEN', data=payload)
+        print "Sent data to Ubidots"
+        print data
+        print {'temperature': fTemp, 'humidity': humidity, 'barometricpressure':pressure }
+        time.sleep(3)
